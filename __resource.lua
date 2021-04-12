166d5ae32da7a21999678fe5c03d8ce7adec9647
Macius <55083897+Macius1337@users.noreply.github.com>
2021-04-12 20:38:42 +0300

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -2,4 +2,9 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'MCS Scripts'
 
-client_scripts '*.lua'
+client_scripts {
+    'drivebyonlyfpsmode.lua',
+    'nodrop.lua',
+    'nohelmet.lua'
+}
+
